ef5b23ac80efefd71b677859f439ba42c771b5dc
FloatingGhost <hannah@coffee-and-dreams.uk>
2026-04-01 17:03:26 +0100

    correct env

--- config/dev.config.exs
+++ config/dev.config.exs
@@ -8,3 +8,7 @@ config :lobber,
   provider: Lobber.Provider.OpenRouter
 
 config :lobber, Lobber.Provider.OpenRouter, model_id: "xiaomi/mimo-v2-flash"
+
+if File.exists?("./config/dev.secret.exs") do
+  import_config "dev.secret.exs"
+end
--- config/runtime.exs
+++ config/runtime.exs
@@ -4,6 +4,6 @@ config :lobber, Lobber.Provider.OpenRouter, api_key: System.get_env("OPENROUTER_
 
 config :lobber, Lobber.Provider.Xiaomi, api_key: System.get_env("XIAOMI_API_KEY")
 
-config :lobber, Lobber.Channels.Discord, bot_token: System.fetch_env!("DISCORD_BOT_TOKEN")
+config :lobber, Lobber.Channels.Discord, bot_token: System.get_env("DISCORD_BOT_TOKEN")
 
 config :lobber, Lobber.Integrations.Perplexity, api_key: System.get_env("PERPLEXITY_API_KEY")
